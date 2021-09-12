fd4ad0836f4861278d1efd746f5b77ddf3f8629d
sacredrel1c <tpek07@gmail.com>
2021-09-12 16:50:04 +0300

    husky test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 const moment = require('moment');
-let vasya;;
+
 let duration: number = 5;
 const timer = document.getElementById('timer');
 const increaseButton = document.getElementById('increase');
@@ -8,27 +8,27 @@ const startButton = document.getElementById('start');
 const title = document.getElementById('title');
 timer.innerText = duration.toString();
 increaseButton.addEventListener('click', () => {
-  duration += 1;
-  timer.innerText = duration.toString();
+    duration += 1;
+    timer.innerText = duration.toString();
 });
 decreaseButton.addEventListener('click', () => {
-  if (duration > 0) {
-    duration -= 1;
-    timer.innerText = duration.toString();
-  }
+    if (duration > 0) {
+        duration -= 1;
+        timer.innerText = duration.toString();
+    }
 });
 startButton.addEventListener('click', () => {
-  increaseButton.parentNode.removeChild(increaseButton);
-  decreaseButton.parentNode.removeChild(decreaseButton);
-  startButton.parentNode.removeChild(startButton);
-  title.innerText = 'Осталось';
-  const interval = 1000;
-  const endTime: number = moment().unix() + duration * 60;
-  setInterval(() => {
-    const currentTime: number = moment().unix();
-    const timeLeft = endTime - currentTime;
-    document.getElementById('timer').innerText = moment
-      .utc(timeLeft * interval)
-      .format('mm:ss');
-  }, interval);
+    increaseButton.parentNode.removeChild(increaseButton);
+    decreaseButton.parentNode.removeChild(decreaseButton);
+    startButton.parentNode.removeChild(startButton);
+    title.innerText = 'Осталось';
+    const interval = 1000;
+    const endTime: number = moment().unix() + duration * 60;
+    setInterval(() => {
+        const currentTime: number = moment().unix();
+        const timeLeft = endTime - currentTime;
+        document.getElementById('timer').innerText = moment
+            .utc(timeLeft * interval)
+            .format('mm:ss');
+    }, interval);
 });
